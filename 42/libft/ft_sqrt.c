910f5f5760b19079e18e8ff2efba1a3ccd2c9cec
Yaniss B <49861295+ybenabdallah@users.noreply.github.com>
2021-03-18 13:52:17 +0100

    Create ft_sqrt.c

---
+++ 42/libft/ft_sqrt.c
@@ -0,0 +1,16 @@
+#include <unistd.h>
+
+int     ft_sqrt(int nb)
+{
+    int i;
+    i = 0;
+    while(i * i < nb)
+    {
+      if(i * i == nb)
+      {
+       return(i); 
+      }
+      i++; 
+    }
+  return(0);
+}
